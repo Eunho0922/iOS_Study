bb097a099883ab3b3dd7bcd965f87fe8b68d43a8
은호 <127753071+Eunho0922@users.noreply.github.com>
2023-11-30 19:20:20 +0900

    ♻️ :: [#1] baseViewController 추가

--- iOS_Study/iOS_Study/Sources/Base/BaseViewController.swift
+++ iOS_Study/iOS_Study/Sources/Base/BaseViewController.swift
@@ -1,8 +1,29 @@
-//
-//  BaseViewController.swift
-//  iOS_Study
-//
-//  Created by 이은호 on 11/30/23.
-//
-
+import UIKit
+import SnapKit
+import Then
 import Foundation
+
+open class BaseVC: UIViewController {
+    
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        layout()
+        attribute()
+        hideKeyboardWhenTappedArround()
+    }
+    
+    open func hideKeyboardWhenTappedArround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard) )
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    open func layout() {}
+    open func attribute() {}
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+
+
